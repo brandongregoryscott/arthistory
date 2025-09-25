89c06f2758409cc0c8099575f0d0ca47632205e3
Brandon Scott <contact@brandonscott.me>
2025-09-25 10:26:41 -0400

    refactor: extract smaller functions for download logic

--- packages/scripts/download-dbs/download-dbs.ts
+++ packages/scripts/download-dbs/download-dbs.ts
@@ -1,20 +1,20 @@
-import { S3 } from "@aws-sdk/client-s3";
+import { S3, _Object } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 import { PARTIAL_DB_PREFIX, S3_BUCKET_NAME } from "../constants/storage";
 import { writeFile } from "node:fs/promises";
 
 dotenv.config();
 
-const main = async () => {
-    const s3 = new S3({
-        endpoint: process.env.AWS_S3_ENDPOINT,
-        region: "auto",
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
-        },
-    });
+const s3 = new S3({
+    endpoint: process.env.AWS_S3_ENDPOINT,
+    region: "auto",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    },
+});
 
+const main = async () => {
     const { Contents: objects = [] } = await s3.listObjectsV2({
         Bucket: S3_BUCKET_NAME,
         Prefix: PARTIAL_DB_PREFIX,
@@ -22,25 +22,28 @@ const main = async () => {
 
     console.log(`Found ${objects.length} partial databases...`);
 
-    await Promise.all(
-        objects.map(async (object) => {
-            const key = object.Key;
-            if (key === undefined) {
-                return;
-            }
+    await downloadObjects(objects);
+};
 
-            const result = await s3.getObject({
-                Bucket: S3_BUCKET_NAME,
-                Key: key,
-            });
-            const body = await result.Body?.transformToByteArray();
-            if (body === undefined) {
-                return;
-            }
+const downloadObjects = async (objects: _Object[]) =>
+    Promise.all(objects.map(downloadObject));
 
-            await writeFile(key, body);
-        })
-    );
+const downloadObject = async (object: _Object) => {
+    const key = object.Key;
+    if (key === undefined) {
+        return;
+    }
+
+    const result = await s3.getObject({
+        Bucket: S3_BUCKET_NAME,
+        Key: key,
+    });
+    const body = await result.Body?.transformToByteArray();
+    if (body === undefined) {
+        return;
+    }
+
+    await writeFile(key, body);
 };
 
 main();
