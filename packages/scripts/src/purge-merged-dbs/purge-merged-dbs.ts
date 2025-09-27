6fcaae1ba739b1fd48d6b43fc5d0b6dfd2c7bfd7
Brandon Scott <contact@brandonscott.me>
2025-09-27 18:36:57 -0400

    feat: scaffold out purge-merged-dbs script

---
+++ packages/scripts/src/purge-merged-dbs/purge-merged-dbs.ts
@@ -0,0 +1,25 @@
+import { PARTIAL_DB_PREFIX, S3_BUCKET_NAME } from "../constants/storage";
+import { getDbFileNames } from "../utils/fs-utils";
+import { s3 } from "../utils/storage-utils";
+
+const main = async () => {
+    const localDbFileNames = await getDbFileNames();
+    console.log(`Found ${localDbFileNames.length} dbs locally`);
+
+    const { Contents: remoteDbObjects = [] } = await s3.listObjectsV2({
+        Bucket: S3_BUCKET_NAME,
+        Prefix: PARTIAL_DB_PREFIX,
+    });
+
+    console.log(`Found ${remoteDbObjects.length} dbs remotely`);
+
+    const remoteDbObjectsToDelete = remoteDbObjects.filter((object) =>
+        localDbFileNames.includes(object.Key ?? "")
+    );
+
+    console.log(
+        `Remote dbs slated for deletion:\n${remoteDbObjectsToDelete.map((object) => object.Key).join("\n")}`
+    );
+};
+
+main();
