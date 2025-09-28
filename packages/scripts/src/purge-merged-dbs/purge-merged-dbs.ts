f6becd8a7e14dc7e1c7a3a00075055201f9f1e9b
Brandon Scott <contact@brandonscott.me>
2025-09-28 11:41:54 -0400

    feat: implement actual deletion w/ confirmation prompt for purge-merged-dbs script

--- packages/scripts/src/purge-merged-dbs/purge-merged-dbs.ts
+++ packages/scripts/src/purge-merged-dbs/purge-merged-dbs.ts
@@ -1,6 +1,10 @@
 import { PARTIAL_DB_PREFIX, BUCKET_NAME } from "../constants/storage";
 import { getDbFileNames } from "../utils/fs-utils";
 import { s3 } from "../utils/storage-utils";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const readlineInterface = readline.createInterface({ input, output });
 
 const main = async () => {
     const localDbFileNames = await getDbFileNames();
@@ -20,6 +24,28 @@ const main = async () => {
     console.log(
         `Remote dbs slated for deletion:\n${remoteDbObjectsToDelete.map((object) => object.Key).join("\n")}`
     );
+
+    const answer = await readlineInterface.question(
+        `Delete ${remoteDbObjectsToDelete.length} objects from bucket '${BUCKET_NAME}'? [y/N] `
+    );
+
+    if (answer.toLowerCase().trim() !== "y") {
+        process.exit(0);
+    }
+
+    readlineInterface.close();
+
+    const label = `Deleted ${remoteDbObjectsToDelete.length} objects`;
+    console.time(label);
+
+    await s3.deleteObjects({
+        Bucket: BUCKET_NAME,
+        Delete: {
+            Objects: remoteDbObjects.map((object) => ({ Key: object.Key })),
+        },
+    });
+
+    console.timeEnd(label);
 };
 
 main();
