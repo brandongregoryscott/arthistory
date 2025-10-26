e36c276677f3fefd98764010cc44b4c37ef7140e
Brandon Scott <contact@brandonscott.me>
2025-10-26 15:52:40 -0400

    wip: retrieve artists + generate sql statements for insertion

--- packages/scripts/src/sync/sync.ts
+++ packages/scripts/src/sync/sync.ts
@@ -1,4 +1,4 @@
-import type { Artist } from "@spotify/web-api-ts-sdk";
+import type { Artist, SpotifyApi } from "@spotify/web-api-ts-sdk";
 import { ARTIST_ID_ALEX_G, ARTIST_ID_DUSTER } from "../constants/spotify";
 import {
     countRows,
@@ -6,7 +6,11 @@ import {
     openDb,
     openSnapshotDb,
 } from "../utils/db-utils";
-import { buildCurrentSpotifyClient } from "../utils/spotify-utils";
+import {
+    buildCurrentSpotifyClient,
+    buildRandomSpotifyClient,
+    isRateLimitError,
+} from "../utils/spotify-utils";
 import type { SQLStatement } from "../types";
 import {
     getCurrentHourIndex,
@@ -18,6 +22,18 @@ import {
     ARTIST_SNAPSHOTS_TABLE_NAME,
 } from "../constants/storage";
 import type { Entity } from "@repo/common";
+import { chunk, flatten, take } from "lodash";
+import { sleep } from "../utils/core-utils";
+
+/**
+ * The maximum number of artist ids that can be requested at once via the Spotify API.
+ */
+const MAX_ARTIST_IDS_PER_REQUEST = 50;
+
+/**
+ * Maximum number of attempts to retry a request before giving up.
+ */
+const MAX_RETRY_ATTEMPTS = 10;
 
 const sync = async () => {
     const timestamp = getRoundedTimestamp();
@@ -26,11 +42,21 @@ const sync = async () => {
     const db = await openSnapshotDb();
     await createArtistSnapshotsTable(db);
 
-    const artistIds = await readArtistIds();
-    const artists = await spotify.artists.get([
-        ARTIST_ID_ALEX_G,
-        ARTIST_ID_DUSTER,
-    ]);
+    const artistIds = take(await readArtistIds(), 3000);
+
+    const artistIdChunks = chunk(artistIds, MAX_ARTIST_IDS_PER_REQUEST);
+
+    const statements = flatten(
+        await Promise.all(
+            artistIdChunks.map(async (artistIdChunk) =>
+                getArtistSnapshots({
+                    artistIds: artistIdChunk,
+                    timestamp,
+                })
+            )
+        )
+    );
+    console.log(statements);
 };
 
 const readArtistIds = async (): Promise<string[]> => {
@@ -47,6 +73,37 @@ const readArtistIds = async (): Promise<string[]> => {
     const ids = rows.map((row) => row.id);
 
     return ids;
+};
+
+interface GetArtistSnapshotsOptions {
+    artistIds: string[];
+    attempt?: number;
+    timestamp: number;
+}
+
+const getArtistSnapshots = async (
+    options: GetArtistSnapshotsOptions
+): Promise<SQLStatement[]> => {
+    const { timestamp, artistIds, attempt = 0 } = options;
+    const spotify =
+        attempt < 5 ? buildCurrentSpotifyClient() : buildRandomSpotifyClient();
+    try {
+        const artists = await spotify.artists.get(artistIds);
+        return artists.map((artist) => buildInsertStatement(artist, timestamp));
+    } catch (error) {
+        if (isRateLimitError(error) && attempt < MAX_RETRY_ATTEMPTS) {
+            const secondsToSleep = 2 ** attempt;
+            await sleep(secondsToSleep * 1000);
+            return getArtistSnapshots({
+                timestamp,
+                artistIds,
+                attempt: attempt + 1,
+            });
+        }
+
+        console.error("Unexpected error retrieving artists", error);
+        return [];
+    }
 };
 
 const buildInsertStatement = (
--- packages/scripts/src/utils/spotify-utils.ts
+++ packages/scripts/src/utils/spotify-utils.ts
@@ -10,13 +10,9 @@ interface BuildSpotifyClientOptions {
 const buildSpotifyClient = (options: BuildSpotifyClientOptions) =>
     SpotifyApi.withClientCredentials(options.clientId, options.clientSecret);
 
-const buildCurrentSpotifyClient = () => {
-    const currentHourIndex = getCurrentHourIndex();
-    // We have 8 client secrets available to use, so spread them across 24 hours for maximum usage
-    const secretIndex = currentHourIndex % 8;
-    const clientId = CLIENT_IDS[secretIndex];
-    const clientSecret = CLIENT_SECRETS[secretIndex];
-
+const buildSpotifyClientBySecretPair = (index: number) => {
+    const clientId = CLIENT_IDS[index];
+    const clientSecret = CLIENT_SECRETS[index];
     if (clientId === undefined || clientSecret === undefined) {
         throw new Error("Client id or secret was unexpectedly undefined");
     }
@@ -24,4 +20,43 @@ const buildCurrentSpotifyClient = () => {
     return buildSpotifyClient({ clientId, clientSecret });
 };
 
-export { buildCurrentSpotifyClient, buildSpotifyClient };
+/**
+ * Returns a Spotify client with the current hour's allocated client id/secret pair
+ */
+const buildCurrentSpotifyClient = () => {
+    const currentHourIndex = getCurrentHourIndex();
+    // We have 8 client secrets available to use, so spread them across 24 hours for maximum usage
+    const secretIndex = currentHourIndex % 8;
+    return buildSpotifyClientBySecretPair(secretIndex);
+};
+
+/**
+ * Returns a Spotify client with a random client id/secret pair
+ */
+const buildRandomSpotifyClient = () => {
+    const max = CLIENT_IDS.length - 1;
+    const index = randomInteger({ min: 0, max });
+
+    return buildSpotifyClientBySecretPair(index);
+};
+
+const isRateLimitError = (error: unknown): boolean =>
+    error instanceof Error &&
+    error.message === "The app has exceeded its rate limits.";
+
+interface RandomIntegerOptions {
+    max: number;
+    min: number;
+}
+
+const randomInteger = (options: RandomIntegerOptions): number => {
+    const { min, max } = options;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export {
+    buildCurrentSpotifyClient,
+    buildRandomSpotifyClient,
+    buildSpotifyClient,
+    isRateLimitError,
+};
