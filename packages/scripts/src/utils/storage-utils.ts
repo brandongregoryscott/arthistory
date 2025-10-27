001d89fe54df8026c7961f6e68c58fd870964a32
Brandon Scott <contact@brandonscott.me>
2025-10-26 21:12:05 -0400

    fix: ensure artist ids db is downloaded before running schedule cron job (#33)

--- packages/scripts/src/utils/storage-utils.ts
+++ packages/scripts/src/utils/storage-utils.ts
@@ -1,3 +1,4 @@
+import type { _Object } from "@aws-sdk/client-s3";
 import { S3 } from "@aws-sdk/client-s3";
 import type { Progress } from "@aws-sdk/lib-storage";
 import { bytesToMb } from "./fs-utils";
@@ -6,6 +7,7 @@ import {
     AWS_S3_ENDPOINT,
     AWS_SECRET_ACCESS_KEY,
 } from "../config";
+import { createWriteStream } from "node:fs";
 
 const s3 = new S3({
     endpoint: AWS_S3_ENDPOINT,
@@ -29,4 +31,26 @@ const logUploadProgress = (progress: Progress) => {
     );
 };
 
-export { logUploadProgress, s3 };
+const downloadObjects = async (keys: string[], bucket: string) =>
+    Promise.all(keys.map((key) => downloadObject(key, bucket)));
+
+const downloadObject = async (key: string, bucket: string) => {
+    const result = await s3.getObject({
+        Bucket: bucket,
+        Key: key,
+    });
+    const objectStream = result.Body?.transformToWebStream();
+    if (objectStream === undefined) {
+        return;
+    }
+
+    const fileStream = createWriteStream(key);
+    const writableStream = new WritableStream({
+        write: (chunk) => {
+            fileStream.write(chunk);
+        },
+    });
+    await objectStream.pipeTo(writableStream);
+};
+
+export { downloadObject, downloadObjects, logUploadProgress, s3 };
