2781a1900c014ade0b7fad59d54709ac0f6ef5fe
Brandon Scott <contact@brandonscott.me>
2025-10-27 19:03:09 -0400

    feat: script for listing dbs in a bucket

---
+++ packages/scripts/src/list-dbs/list-dbs.ts
@@ -0,0 +1,25 @@
+import { bytesToMb } from "../utils/fs-utils";
+import { listObjects } from "../utils/storage-utils";
+
+interface ListDbsOptions {
+    bucket: string;
+    prefix?: string;
+}
+
+const listDbs = async (options: ListDbsOptions) => {
+    const { bucket, prefix } = options;
+    const objects = await listObjects({ prefix, bucket });
+
+    console.log("| Key | LastModified | Size |");
+    console.log("| -- | -- | -- |");
+    console.log(
+        objects
+            .map(
+                (object) =>
+                    `| ${object.Key} | ${object.LastModified?.toISOString()} | ${bytesToMb(object.Size ?? 0)} |`
+            )
+            .join("\n")
+    );
+};
+
+export { listDbs };
