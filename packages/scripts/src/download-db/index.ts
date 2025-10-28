a7151a79c768ba5ee67245d8c237191c15158c2b
Brandon Scott <contact@brandonscott.me>
2025-10-28 18:52:56 -0400

    refactor: use prepared option to define choices for bucket name for validation

--- packages/scripts/src/download-db/index.ts
+++ packages/scripts/src/download-db/index.ts
@@ -1,14 +1,17 @@
-import { program } from "commander";
+import { program, Option } from "commander";
 import { BucketName } from "../constants/storage";
 import type { DownloadDbOptions } from "./download-db";
 import { downloadDb } from "./download-db";
 
 program.option("--prefix <prefix>", "Object prefix to filter dbs by");
 
-program.option(
-    "--bucket <bucket>",
-    "Name of the bucket to download the object from",
-    BucketName.Snapshots
+program.addOption(
+    new Option(
+        "--bucket <bucket>",
+        "Name of the bucket to download the object from"
+    )
+        .choices(Object.values(BucketName))
+        .default(BucketName.Snapshots)
 );
 
 program.requiredOption("--key <key>", "Key of the object to download");
